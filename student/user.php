65a7af8491e0dadb341980c49991f472327776f9
cliffordmatamba <matambaclifford@gmail.com>
2020-07-07 17:21:17 +0200

    Add files via upload

--- student/user.php
+++ student/user.php
@@ -5,7 +5,7 @@
 
 <div class="container-fluid jumbo_user mx-auto">
 
-  <h2 class="text-center">Employee Space</h2>
+  <h2 class="text-center">Myspace</h2>
 
    <br>
    <h1 class="text-center" >Dashboard</h1>
@@ -14,15 +14,22 @@
  <div class="rows">
  
   <div class="col-sm-6 color_white col-sm-6-mod">
-    <h3 class="text-center">Check EID </h3>
+    <h3 class="text-center">Check ID </h3>
 	<br>
-    <a class="a_color"  href="vieweid.php"><button type="button" class="btn btn-primary btn-lg">Find EID</button></a>
+    <a class="a_color"  href="vieweid.php"><button type="button" class="btn btn-primary btn-lg">Find UID</button></a>
   
   </div>
   <div class="col-sm-6 color_white col-sm-6-mod">
    <h3 class="text-center">View Attendance</h3>
 	<br>
    <a class="a_color" href="viewattendanceuser.php"><button type="button" class="btn btn-primary btn-lg">Attendance</button></a>
+    
+  </div>
+
+  <div class="col-sm-6 color_white col-sm-6-mod">
+   <h3 class="text-center">Temperature</h3>
+	<br>
+   <a class="a_color" href="viewtempuser.php"><button type="button" class="btn btn-primary btn-lg">temperature</button></a>
     
   </div>
   
